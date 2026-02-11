0f5b9db989a02a52ce4116927d2b0c2d9740d928
Ilya Danilkin <nezorflame@gmail.com>
2026-02-11 20:42:22 +0400

    Fix the LUA errors by just hiding the frame

--- RemoveSellPriceNG.lua
+++ RemoveSellPriceNG.lua
@@ -1,21 +1,15 @@
--- Sourced from GameTooltip_ClearMoney:
--- https://github.com/Gethe/wow-ui-source/blob/live/Interface/FrameXML/GameTooltip.lua#L474
-function RemoveSellPrice(tooltip, lineData)
-    if (not tooltip.shownMoneyFrames) then
-        return;
-    end
-
-    local moneyFrame;
-    for i = 1, tooltip.shownMoneyFrames do
-        moneyFrame = _G[tooltip:GetName() .. "MoneyFrame" .. i];
-        if (moneyFrame) then
-            moneyFrame:Hide();
-            MoneyFrame_SetType(moneyFrame, "STATIC");
+-- Midnight changes:
+-- Using the OnTooltipAddMoney event which is safer for the UI stack
+local function RemoveSellPrice(tooltip)
+    if tooltip.shownMoneyFrames then
+        for i = 1, tooltip.shownMoneyFrames do
+            local moneyFrame = _G[tooltip:GetName() .. "MoneyFrame" .. i]
+            if moneyFrame then
+                -- Only hiding the frame visually, not modifying it now:
+                moneyFrame:Hide()
+            end
         end
     end
-    tooltip.shownMoneyFrames = nil
 end
 
--- The new way to add tooltip changes as per 10.0.2 API changes:
--- https://wowpedia.fandom.com/wiki/Patch_10.0.2/API_changes
 TooltipDataProcessor.AddLinePostCall(Enum.TooltipDataLineType.SellPrice, RemoveSellPrice)
